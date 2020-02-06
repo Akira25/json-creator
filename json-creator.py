c86f8ff3f0019965956f0771e6def656f52fc475
Martin Hübner <martin.hubner@web.de>
2020-02-06 23:40:44 +0100

    *option detection: recognise command-line options correctly and apply them right. *dictionary: implement function for creation of routerstring-dictionaries. *tidy up some code.

--- json-creator.py
+++ json-creator.py
@@ -1,29 +1,37 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
-from datetime import date
+from datetime import datetime as date
 import urllib.request
+import os
 import json
 import sys
 import re
 
+"""
+This script takes all routers listed in wiki of Freifunk Berlin and fetches
+their image dowload links. The links are stored in a json-file. Thus they will 
+be easily accessible by autoupdate
+"""
+
 
 def print_help():
     print("""Usage:
-    \t./generate_release.py <arg> [arg]
+    \t./json-creator.py <arg> [arg]
        
 Options:
  -r\tgenerate release
  -t\tgenerate testing
- -d\tgenerate developement
+ -d\tgenerate development
  
+ -s\tgenerate an (empty) strings-dictionary file. 
  -a\twrite the complete list fetched from wiki (just for debugging)
  -h\tprint this message
  """)
 
 
-#gets all links from the given page and returns them as list
 def get_links(url):
+    #gets all links from the given page and returns them as list
     html_page = urllib.request.urlopen(url,None)
     soup = BeautifulSoup(html_page, "html.parser")
     links = []
@@ -48,6 +56,11 @@ def sort_release(r_dict, testing):
     # store the routers with release status at seperate list.
     stables = {}
     for router in r_dict:
+        #continue, if current elem is creation date string.
+        if router == "date":
+            stables[router] = r_dict.get(router)
+            continue
+            
         # get release status from download link: take first link from 
         # router and split by '/'.  fourth elem will be 'stable' or 'unstable'
         key = list(r_dict[router])[0]
@@ -70,6 +83,11 @@ def sort_development(r_dict):
     # store the routers with release status at seperate list.
     devs = {}
     for router in r_dict:
+        #continue, if current elem is creation date string.
+        if router == "date":
+            devs[router] = r_dict.get(router)
+            continue
+        
         # get release status from download link: take first link from 
         # router and split by '/'.  fourth elem will be 'stable' or 'unstable'
         key = list(r_dict[router])[0]
@@ -86,10 +104,31 @@ def sort_development(r_dict):
 
 def write_all(r_dict):
     write_file(r_dict,"DEBUG_all_routers.json")
+    
+
+def generate_dict(r_dict):
+    #generate a dictionary. This transfers the router names from 
+    #buildbot to the strings reffered to by the routers.
+    router_list = list(r_dict)
+    
+    name_dict = {}
+    for router in router_list:
+        name_dict[router] = " "
+    
+    
+    #if there is already a file, make a backup.
+    #filename includes date of backup
+    if os.path.isfile("routernames_dict.json"):
+        today = date.now().strftime("%Y-%m-%d_%H-%M_")
+        current_dir = os.getcwd()
+        os.rename(current_dir+"/routernames_dict.json", current_dir+"/"+today+"routernames_dict.json.bkp")
+        
+    #write the empty dict to file. The original strings musst
+    #be inserted by hand!
+    write_file(name_dict,"routernames_dict.json")
 
 
 def fetch_routers():
-    
     router_links = get_links("https://wiki.freifunk.net/Berlin:Firmware")
     
     #let there only be the links pointing to util.berlin.freifunk.net
@@ -128,7 +167,7 @@ def fetch_routers():
                 firmware_type = str(link).split("/")[-2]
                 images[firmware_type] = link
         
-        #if there weren't any links (due to developement-firmware), try to get the image-links again.
+        #if there weren't any links (due to development-firmware), try to get the image-links again.
         
         if len(images) == 0:
             image_links = get_links(router_link+"&complete=true")
@@ -144,21 +183,40 @@ def fetch_routers():
         if len(images) != 0:
             routers[router_name] = images
         else:
-            print(router_name+" did not contain links. It won't be added.")
+            err_string = router_name+" did not contain any links. It won't be included!"
+            print(err_string)
+            #stderr do print only at the and of the output. I prefer it to be in between
+            #the other routers...
+            #sys.stderr.write(err_string)
     
     return routers
     
     """
     #DEBUGGING: load json-file with debugging-list
+    print("fetching routers")
     g = open("routers.json.bkp", "r")
     dictionary = g.read()
     data = {}
     data = json.loads(dictionary)
     data.pop("date")
     g.close()
+    print("fetching done.")
     
     return data
     """
+
+
+def invalid_invokation(opt):
+    errstring = "invalid option: "+opt+"\nStart script with -h for further information\n"
+    sys.stderr.write(errstring)
+    exit(2)
+
+def check_cmd_option(option):
+    option_list = ["-r","-t","-d","-s","-a","-h"]
+
+    if option not in option_list:
+        invalid_invokation(option)
+        
 
 
 """
@@ -169,22 +227,29 @@ Main-Programm
 ##
 """
 
-#get arguments and start functions
+"""
+#read the options and start the function respectively
+"""
+
 argc = len(sys.argv)
+#exit, if no options are given
+if argc == 1:
+    print_help()
+    exit(2)
+
+
 arg_index = 1
+func = str(sys.argv[arg_index])
+check_cmd_option(func)
+if func == "-h":
+    print_help()
+    exit(0)
+
+complete_router_list = fetch_routers()
 
 while arg_index < argc:
     func = str(sys.argv[arg_index])
-    
-    if func == None:
-        sys.stderr.write("invalid argument! Start script with -h\n")
-        exit()
-    
-    if func == "print_help()":
-        print_help()
-        exit()
-    
-    complete_router_list = fetch_routers()
+    check_cmd_option(func)
     
     #True and False control the name of the output file: False will generate a "release.json"
     #True will generate a "testing.json"
@@ -194,12 +259,12 @@ while arg_index < argc:
         sort_release(complete_router_list,True)
     elif func == "-d":
         sort_development(complete_router_list)
+    elif func == "-s":
+        generate_dict(complete_router_list)
     elif func == "-a":
         write_all(complete_router_list)
     else:
-        errstring = "invalid option: "+func+"\n"
-        sys.stderr.write(errstring)
-        exit(False)
+        invalid_invokation(func)
 
     arg_index +=1
-    
+
